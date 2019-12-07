4c3d6849ae70713c176f1a0dd6e4f7c94f3127dd
yasir <yasir.solo@yahoo.com>
2019-12-07 08:55:37 +0700

    update

--- app/services/env.service.ts
+++ app/services/env.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
-  API_URL = 'http://localhost:8000/api/';
+  // API_URL = 'http://localhost:8000/api/';
+  API_URL = 'http://originalindonesia.id/api/api/';
   constructor() { }
 }
