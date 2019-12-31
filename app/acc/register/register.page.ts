709a93398f0518ae388cabb982d115f6051fe7b8
yasir <yasir.solo@yahoo.com>
2019-12-31 16:03:04 +0700

    update

--- app/acc/register/register.page.ts
+++ app/acc/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
+  isErrorEmail = false;
+  emailErrorMsg = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+  emailFormSubmit(form: NgForm){
+    console.log("isi email submit >>>")
+    console.log(form.value.username);
+    this.isErrorEmail = !form.value.username.includes("@");
+    if (this.isErrorEmail){
+      this.emailErrorMsg = 'email tidak valid.';
+    }else{
+      this.emailErrorMsg = '';
+    }
+  }
 }
